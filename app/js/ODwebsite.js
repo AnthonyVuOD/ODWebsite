455d3d6d88af68ab2ee9f1ccaa8a5f1f40869bb2
Tony Vu <AnthonyVuOD@gmail.com>
2017-01-29 18:39:05 -0600

    final

--- app/js/ODwebsite.js
+++ app/js/ODwebsite.js
@@ -2,7 +2,8 @@ function initMap() {
   var stPete = {lat: 27.773056, lng: -82.639999};
   var map = new google.maps.Map(document.getElementById('map'),{
     zoom: 10,
-    center: stPete
+    center: stPete,
+    scrollwheel:false
   });
   var marker = new google.maps.Marker({
     position: stPete,
